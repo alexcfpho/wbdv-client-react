d591f722fb7ccb9e4469c5fc576b5abbec697b3b
Alex Pho <pho.a@husky.neu.edu>
2021-02-19 02:11:05 -0500

    link to editor, deletecourse, and latest changes from jose.

--- src/components/course-card.js
+++ src/components/course-card.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
 
-const CourseCard = ({course}) =>
+const CourseCard = ({deleteCourse, course, title, owner, lastModified}) =>
     <div className="col-4">
         <div className="card">
+            <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png" className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{course.title}</h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                     content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-                <i className="fas fa-trash"></i>
+                <img src={``}/>
+                <Link to={"/courses/editor"} className="btn btn-primary">{title}</Link>
+                <FontAwesomeIcon icon={"trash"} size={"lg"}
+                    onClick={() => deleteCourse(course)}/>
             </div>
         </div>
     </div>
