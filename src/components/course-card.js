9a2a84b41ab0d38ffa80d65c43d52ca16a02a613
Alex Pho <pho.a@husky.neu.edu>
2021-02-22 02:44:07 -0500

    add updateCourse & state for pencil on hover

--- src/components/course-card.js
+++ src/components/course-card.js
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from 'react-router-dom';
 import {Col} from "react-bootstrap";
 
-const CourseCard = ({deleteCourse, course, title, owner, lastModified}) =>
-    <Col xs={12} sm={6} md={4} lg={3} xl={2}>
-        <div className="card mt-4">
-            <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png" className="card-img-top"
-                 alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-                <img src={``}/>
-                <Link to={"/courses/editor"} className="btn btn-primary">{title}</Link>
-                <FontAwesomeIcon icon={"trash"} size={"lg"}
-                                 onClick={() => deleteCourse(course)}/>
+const CourseCard = (
+    {
+        deleteCourse,
+        updateCourse,
+        course,
+        title,
+    }) => {
+    const [editing, setEditing] = useState(false)
+    const [newTitle, setNewTitle] = useState(title)
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(true)
+    }
+
+    const onLeave = () => {
+        setHover(false)
+    }
+
+    const saveTitle = () => {
+        setEditing(false)
+        const newCourse = {
+            ...course,
+            title: newTitle
+        }
+        updateCourse(newCourse)
+    }
+    return (
+        <Col xs={12} sm={6} md={4} lg={3} xl={2}>
+            <div className="card mt-4"
+                 onMouseEnter={onHover} onMouseLeave={onLeave}>
+                <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
+                     className="card-img-top"
+                     alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's
+                        content.</p>
+                    <img src={``}/>
+                    <Link to={"/courses/editor"} className="btn btn-primary">{title}</Link>
+                    {hover ? <FontAwesomeIcon icon={"edit"} size={"lg"} pull={"right"} className={"mt-3"}
+                                              onClick={() => deleteCourse(course)}/> : null}
+                </div>
             </div>
-        </div>
-    </Col>
+        </Col>
+    )
+}
 
 export default CourseCard
