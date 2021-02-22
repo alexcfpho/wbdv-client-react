3bbbd63ef9c4c2851a4feff2c051564260e6d06a
Alex Pho <pho.a@husky.neu.edu>
2021-02-22 16:36:24 -0500

    clear form title after creating course

--- src/components/course-top-bar.js
+++ src/components/course-top-bar.js
@@ -14,6 +14,7 @@ const CourseTopBar = (
             lastModified: new Date().toLocaleDateString()
         }
         createCourse(aCourse)
+        setNewTitle("")
     }
     return (
         <div className="mt-2 no-gutters">
