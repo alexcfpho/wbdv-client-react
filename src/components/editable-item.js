aa84b829151d0c9f3c1ef541fc7fd765de09aac7
Alex Pho <pho.a@husky.neu.edu>
2021-03-09 00:14:32 -0500

    cleanup and add highlight to edited

--- src/components/editable-item.js
+++ src/components/editable-item.js
@@ -4,14 +4,11 @@ import {Link} from "react-router-dom";
 
 const EditableItem = (
     {
-        // This to is overridden when passed the prop depending where the Component is invoked.
         to = "/somewhere/to/go",
         deleteItem,
         updateItem,
-        // @TODO WIP
-        // findItem,
         item = {title: "Some Title", _id: "ABC"},
-        active,
+        active
     }) => {
     const [editing, setEditing] = useState(false)
     const [cachedItem, setCachedItem] = useState(item)
@@ -22,7 +19,6 @@ const EditableItem = (
                 <>
                     <Link className={`nav-link ${active ? 'active' : ''} `} to={to}>
                         {item.title}
-                        {/*{JSON.stringify(active)}*/}
                         <FontAwesomeIcon icon={"edit"} onClick={() => setEditing(true)} pull={"right"}/>
                     </Link>
                 </>
@@ -30,23 +26,23 @@ const EditableItem = (
             {
                 editing &&
                 <>
-                    <input
-                        onChange={(e) =>
-                            setCachedItem({
-                                ...cachedItem,
-                                title: e.target.value
-                            })}
-                        value={cachedItem.title}
-                    />
-                    <FontAwesomeIcon icon={"check"} pull={"right"}
-                                     onClick={() => {
-                                         setEditing(false);
-                                         updateItem(cachedItem)
-                                         // @TODO WIP
-                                         // findItem(item._id)
-                                     }}/>
-                    <FontAwesomeIcon icon={"times"} pull={"right"}
-                                     onClick={() => deleteItem(item)}/>
+                    <div className="col-12 btn-primary btn p-3">
+                        <input
+                            onChange={(e) =>
+                                setCachedItem({
+                                    ...cachedItem,
+                                    title: e.target.value
+                                })}
+                            value={cachedItem.title}
+                        />
+                        <FontAwesomeIcon icon={"check"} pull={"right"}
+                                         onClick={() => {
+                                             setEditing(false);
+                                             updateItem(cachedItem)
+                                         }}/>
+                        <FontAwesomeIcon icon={"times"} pull={"right"}
+                                         onClick={() => deleteItem(item)}/>
+                    </div>
                 </>
             }
         </>
