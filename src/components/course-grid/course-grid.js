00a569bde446cbbe2ef9693cf132c4257564a4b2
Alex Pho <pho.a@husky.neu.edu>
2021-03-09 00:16:16 -0500

    added explicit return

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -6,38 +6,46 @@ import {Row} from "react-bootstrap";
 
 import './course-grid.css'
 
-const CourseGrid = ({courses, deleteCourse, updateCourse}) =>
-    <div className="my-2">
-        <div className="text-right">
-            <div className={"row text-left"}>
-                <div className={"col-6"}>
-                    <h4>Recent Documents</h4>
+const CourseGrid = (
+    {
+        courses,
+        deleteCourse,
+        updateCourse
+    }) => {
+    return (
+        <div className="my-2">
+            <div className="text-right">
+                <div className={"row text-left"}>
+                    <div className={"col-6"}>
+                        <h4>Recent Documents</h4>
+                    </div>
+                    <div className={"col-3 d-none d-md-inline"}>
+                        <h4>Owned by me</h4>
+                    </div>
                 </div>
-                <div className={"col-3 d-none d-md-inline"}>
-                    <h4>Owned by me</h4>
-                </div>
+                <FontAwesomeIcon icon={"folder"} order={1} size={"lg"} className="mr-3"/>
+                <FontAwesomeIcon icon={"sort-alpha-up"} order={2} size={"lg"} className="mr-3"/>
+                <Link to={"/courses/table"}>
+                    <FontAwesomeIcon icon={"list"} order={3} size={"lg"} className="mr-1"/>
+                </Link>
             </div>
-            <FontAwesomeIcon icon={"folder"} order={1} size={"lg"} className="mr-3"/>
-            <FontAwesomeIcon icon={"sort-alpha-up"} order={2} size={"lg"} className="mr-3"/>
-            <Link to={"/courses/table"}>
-                <FontAwesomeIcon icon={"list"} order={3} size={"lg"} className="mr-1"/>
-            </Link>
+            <Row>
+                {
+                    courses.map((course, index) =>
+                        <CourseCard
+                            deleteCourse={deleteCourse}
+                            updateCourse={updateCourse}
+                            key={index}
+                            course={course}
+                            title={course.title}
+                            owner={course.owner}
+                            lastModified={course.lastModified}
+                        />
+                    )
+                }
+            </Row>
         </div>
-        <Row>
-            {
-                courses.map((course, index) =>
-                    <CourseCard
-                        deleteCourse={deleteCourse}
-                        updateCourse={updateCourse}
-                        key={index}
-                        course={course}
-                        title={course.title}
-                        owner={course.owner}
-                        lastModified={course.lastModified}
-                    />
-                )
-            }
-        </Row>
-    </div>
+    )
+}
 
 export default CourseGrid
