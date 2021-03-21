c1a59ce821ccd60b4970b0af163f2383b57661f0
Alex Pho <pho.a@husky.neu.edu>
2021-03-21 10:15:50 -0400

    dropdown types for different widgets

---
+++ src/components/course-editor/widgets/widget-type.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const WidgetType = ({widgetCache, setWidgetCache}) =>
+    <>
+        <select className="form-control"
+                value={widgetCache.type}
+                placeholder={"Widget Type"}
+                onChange={(e) => {
+                    if (e.target.value === "HEADING" || e.target.value === "PARAGRAPH") {
+                        setWidgetCache({...widgetCache, type: e.target.value})
+                    } else {
+                        alert("Widget type is not yet supported!")
+                    }
+                }}>
+            <option value={"HEADING"}>Heading</option>
+            <option value={"PARAGRAPH"}>Paragraph</option>
+            <option value={"VIDEO"}>Video</option>
+            <option value={"IMAGE"}>Image</option>
+            <option value={"LINK"}>Link</option>
+            <option value={"LIST"}>List</option>
+            <option value={"HTML"}>HTML</option>
+        </select>
+    </>
+
+export default WidgetType
