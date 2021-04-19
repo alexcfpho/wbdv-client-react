e429a255370dce5453fdb626777dc5d8e7a6db1a
Alex Pho <pho.a@husky.neu.edu>
2021-04-19 09:27:19 -0400

    fix react env to include app slug

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,8 +1,8 @@
 // const QUIZZES_LOCAL_URL = "http://localhost:4000/api/quizzes"
 // const QUESTIONS_LOCAL_URL = "http://localhost:4000/api/questions"
 
-const QUIZZES_REMOTE_URL = process.env.REACT_QUIZZES_URL
-const QUESTIONS_REMOTE_URL = process.env.REACT_QUESTIONS_URL
+const QUIZZES_REMOTE_URL = process.env.REACT_APP_QUIZZES_URL
+const QUESTIONS_REMOTE_URL = process.env.REACT_APP_QUESTIONS_URL
 
 export const findQuestionsForQuiz = (qzid) =>
     fetch(`${QUIZZES_REMOTE_URL}/${qzid}/questions`)
