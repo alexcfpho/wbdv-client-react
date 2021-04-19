47cc1c0e7cc1e1d1dbb761a936d5e54acb0cc5bd
Alex Pho <pho.a@husky.neu.edu>
2021-04-19 01:47:38 -0400

    use env var for quizzes and questions

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,16 +1,19 @@
-const QUIZZES_LOCAL_URL = "http://localhost:4000/api/quizzes"
-const QUESTIONS_LOCAL_URL = "http://localhost:4000/api/questions"
+// const QUIZZES_LOCAL_URL = "http://localhost:4000/api/quizzes"
+// const QUESTIONS_LOCAL_URL = "http://localhost:4000/api/questions"
+
+const QUIZZES_REMOTE_URL = process.env.REACT_QUIZZES_URL
+const QUESTIONS_REMOTE_URL = process.env.REACT_QUESTIONS_URL
 
 export const findQuestionsForQuiz = (qzid) =>
-    fetch(`${QUIZZES_LOCAL_URL}/${qzid}/questions`)
+    fetch(`${QUIZZES_REMOTE_URL}/${qzid}/questions`)
         .then(response => response.json())
 
 export const findAllQuestions = () =>
-    fetch(`${QUESTIONS_LOCAL_URL}`)
+    fetch(`${QUESTIONS_REMOTE_URL}`)
         .then(response => response.json())
 
 export const findQuestionById = (qid) =>
-    fetch(`${QUESTIONS_LOCAL_URL}/${qid}`)
+    fetch(`${QUESTIONS_REMOTE_URL}/${qid}`)
         .then(response => response.json())
 
 const api = {
--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -1,15 +1,16 @@
-const QUIZZES_LOCAL_URL = "http://localhost:4000/api/quizzes"
+// const QUIZZES_LOCAL_URL = "http://localhost:4000/api/quizzes"
+const QUIZZES_REMOTE_URL = process.env.REACT_APP_QUIZZES_URL
 
 export const findAllQuizzes = () =>
-    fetch(`${QUIZZES_LOCAL_URL}`)
+    fetch(`${QUIZZES_REMOTE_URL}`)
         .then(response => response.json())
 
 export const findQuizById = (qzid) =>
-    fetch(`${QUIZZES_LOCAL_URL}/${qzid}`)
+    fetch(`${QUIZZES_REMOTE_URL}/${qzid}`)
         .then(response => response.json())
 
 export const submitQuiz = (qzid, questions) =>
-    fetch(`${QUIZZES_LOCAL_URL}/${qzid}/attempts`, {
+    fetch(`${QUIZZES_REMOTE_URL}/${qzid}/attempts`, {
         method: 'POST',
         body: JSON.stringify(questions),
         headers: {
@@ -19,7 +20,7 @@ export const submitQuiz = (qzid, questions) =>
         .then(result => console.log(result))
 
 export const findAttemptsForQuiz = (qzid) =>
-    fetch(`${QUIZZES_LOCAL_URL}/${qzid}/attempts`)
+    fetch(`${QUIZZES_REMOTE_URL}/${qzid}/attempts`)
         .then(response => response.json())
 
 const api = {
